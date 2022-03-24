ebd0b5a17abc47ae57e376cd2f21267d065f5945
Muhammad Kamran <mkamran1967@gmail.com>
2022-03-23 22:50:45 -0700

    Alembic setup and revisions init

--- alembic/env.py
+++ alembic/env.py
@@ -4,15 +4,16 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from ..app.models import Base # NOTE import from models not database file. This allows alembic to read our models
-from ..app.config import settings
+from app.models import Base # NOTE import from models not database file. This allows alembic to read our models
+from app.config import settings
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+SQLALCHEMY_DATABASE_URL = f'postgresql+psycopg2://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("slqalchemy.url", SQLALCHEMY_DATABASE_URL) # This config setting is overriding the url link in .ini file
+# This config setting is overriding the url link in .ini file
+config.set_main_option("sqlalchemy.url", SQLALCHEMY_DATABASE_URL) 
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
---
+++ alembic/versions/165265a1b5ab_add_content_column_to_post_table.py
@@ -0,0 +1,24 @@
+"""Add content column to post table
+
+Revision ID: 165265a1b5ab
+Revises: aab37bb2fd8a
+Create Date: 2022-03-23 22:30:37.101667
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '165265a1b5ab'
+down_revision = 'aab37bb2fd8a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
